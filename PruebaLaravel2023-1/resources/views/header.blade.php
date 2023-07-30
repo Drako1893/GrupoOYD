60f5522ae8b0a4ae7dee9a06600c1fe440c59a23
Drako <drakosuarezs@gmail.com>
2023-07-29 21:21:30 -0500

    Changes to header and layout

--- PruebaLaravel2023-1/resources/views/header.blade.php
+++ PruebaLaravel2023-1/resources/views/header.blade.php
@@ -1,1 +1,51 @@
-<p>header</p>
+<div class="bg-color-">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img class="d-inline-block align-text-top" src="{{ asset('images/logoOYDWhite.png') }}" alt="OYD" width="auto" height="75">
+                
+            </a>
+            <a class="navbar-brand" href="#">
+                <span class="navbar-brand mb-0 h1 "><b class="text-color-1">GRUPO OYD</b></span>
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon" style="color: white;"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#"><b class="text-color-1">Inicio</b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#"><b class="text-color-1">Contactanos</b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#"><b class="text-color-1">Sobre Nosotros</b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#"><b class="text-color-1">Proyectos</b></a>
+                </li>
+            <!--
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            -->
+            </ul>
+            </div>
+        </div>
+    </nav>
+</div>
